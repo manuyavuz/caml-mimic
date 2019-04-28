5a70f03dd3af2c41b6127062bb1d20e13d184650
Yavuz Nuzumlali <yavuz.nuzumlali@yale.edu>
2019-04-27 21:27:57 -0400

    Update constant setting

--- constants.py
+++ constants.py
@@ -1,10 +1,17 @@
 PAD_CHAR = "**PAD**"
+
 EMBEDDING_SIZE = 100
 MAX_LENGTH = 2500
 
-#where you want to save any models you may train
-MODEL_DIR = '/data/corpora/mimic/experiments/caml-mimic/models'
+from pathlib import Path
+path = Path(__file__).parent.absolute()
 
-DATA_DIR = '/data/corpora/mimic/experiments/caml-mimic/mimicdata'
-MIMIC_3_DIR = '/data/corpora/mimic/experiments/caml-mimic/mimicdata/mimic3'
-MIMIC_2_DIR = '/data/corpora/mimic/experiments/caml-mimic/mimicdata/mimic2'
+#where you want to save any models you may train
+MODEL_DIR = path / 'models/'
+
+# DATA_DIR = '/data/corpora/mimic/experiments/caml-mimic/mimicdata/'
+DATA_DIR = path / 'mimicdata/'
+
+
+MIMIC_3_DIR = DATA_DIR / 'mimic3'
+MIMIC_2_DIR = DATA_DIR / 'mimic2'
